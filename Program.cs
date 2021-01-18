f83bb681f94e06802a8b0c38f14bac3fb3e4e4ac
uchima <jaso0125@gmail.com>
2021-01-18 19:04:14 +0900

    【C#Tips #13】反復処理(foreach文)について

--- Program.cs
+++ Program.cs
@@ -1,9 +1,25 @@
-﻿namespace CSharp
+﻿using System;
+
+namespace CSharp
 {
 	internal class Program
 	{
 		private static void Main(string[] args)
 		{
+			//foreach文
+			var fruits = new string[] { "バナナ", "オレンジ", "りんご", "メロン" };
+
+			Console.WriteLine("------------foreach文--------------");
+
+			foreach (var fruit in fruits)
+			{
+				Console.WriteLine(fruit); // 順に「バナナ」「オレンジ」「りんご」「メロン」を出力
+			}
+			// 4回ループしたらforeach文を抜ける
+			Console.WriteLine("ループ終わり");
+			Console.WriteLine("-----------------------------------");
+
+			Console.Read();
 		}
 	}
 }
