fc9d52e85ac04e6c03419f07f2ed2ef9f14b9a6a
uchima <jaso0125@gmail.com>
2021-02-02 21:10:12 +0900

    【C#Tips #25】usingディレクティブ+ファイル/フォルダ操作について①

--- Program.cs
+++ Program.cs
@@ -1,9 +1,39 @@
-﻿namespace CSharp
+﻿using System.IO; // usingディレクティブ使ってライブラリを宣言
+using System;
+
+namespace CSharp
 {
-	internal class Program
+	public class Program
 	{
 		private static void Main(string[] args)
 		{
+			var folderPath = @"C:\C#TIPS\"; // フォルダーが存在するパス
+			var filePath = @"C:\C#TIPS\file1.txt"; // ファイルが存在するパス
+
+			// フォルダの存在確認(Directory.Exists)
+			Console.WriteLine("------------Directory.Exists-------------");
+			if (Directory.Exists(folderPath))
+			{
+				Console.WriteLine("フォルダが見つかりました！");
+			}
+			else
+			{
+				Console.WriteLine("フォルダが存在しません。");
+			}
+			Console.WriteLine("-----------------------------------------");
+
+			// ファイルの存在確認(File.Exists)
+			Console.WriteLine("---------------File.Exists---------------");
+			if (File.Exists(filePath))
+			{
+				Console.WriteLine("ファイルが見つかりました！");
+			}
+			else
+			{
+				Console.WriteLine("ファイルが存在しません。");
+			}
+			Console.WriteLine("-----------------------------------------");
+			Console.Read();
 		}
 	}
 }
