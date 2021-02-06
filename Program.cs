aa44215bc3ac4a83a7595cc4608638d488901d5c
uchima <jaso0125@gmail.com>
2021-02-06 18:55:24 +0900

    【C#Tips #30】テキストファイルの書き込みについて

--- Program.cs
+++ Program.cs
@@ -1,9 +1,16 @@
-﻿namespace CSharp
+﻿using System.IO;
+
+namespace CSharp
 {
 	internal class Program
 	{
 		private static void Main(string[] args)
 		{
+			// テキストファイルの書き込み
+			using (var text = new StreamWriter(@"C:\C#TIPS\TIPS_30\text.txt", true))
+			{
+				text.WriteLine("こんにちは");
+			}
 		}
 	}
 }
