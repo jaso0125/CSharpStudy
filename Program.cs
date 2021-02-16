eb0b451b6edffcc5e4695c5fb054833f249ea3eb
uchima <jaso0125@gmail.com>
2021-02-16 21:50:28 +0900

    【C#Tips #39】クラスの作成について

--- Program.cs
+++ Program.cs
@@ -1,8 +1,33 @@
 ﻿namespace CSharp
 {
-	internal class Program
+	public class Program
 	{
 		private static void Main(string[] args)
+		{
+			var car = new Car(); // Carクラスによるインスタンス化
+
+			car.CarColor = "Blue"; // プロパティの設定
+			car.Run(); // メソッドの実行
+		}
+	}
+
+	// 車クラス
+	public class Car
+	{
+		public string _carColor; // フィールドの定義
+
+		public string CarColor // プロパティの定義
+		{
+			get => _carColor;
+			set => _carColor = value;
+		}
+
+		public Car() // コンストラクターの定義
+		{
+			// インスタンス化時に最初に呼ばれる
+		}
+
+		public void Run() // メソッドの定義
 		{
 		}
 	}
