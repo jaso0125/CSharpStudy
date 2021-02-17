63cbbe4fdaf024cb17600f69b4fca65e767e9f44
uchima <jaso0125@gmail.com>
2021-02-18 06:28:14 +0900

    【C#Tips #40】フィールドについて

--- Program.cs
+++ Program.cs
@@ -1,9 +1,26 @@
 ﻿namespace CSharp
 {
-	internal class Program
+	public class Program
 	{
 		private static void Main(string[] args)
 		{
+			var car1 = new Car();
+
+			car1.carHeight = 1.65;
+			car1.carWidth = 1.48;
+			car1.carColor = "Red"; // privateなのでエラーが発生する
 		}
+	}
+
+	public class Car
+	{
+		// 車の高さ
+		public double carHeight;
+
+		// 車の幅
+		public double carWidth;
+
+		// 車の色
+		private string carColor;
 	}
 }
